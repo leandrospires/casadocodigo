c88a2db5554267e61b2faf0132cace50c746df63
Leandro Pires <leandro.spires.br@gmail.com>
2020-08-04 21:41:01 -0300

    Conclusão do pgto com Thread

--- src/main/java/br/com/casadocodigo/loja/service/PagamentoService.java
+++ src/main/java/br/com/casadocodigo/loja/service/PagamentoService.java
@@ -1,12 +1,17 @@
 package br.com.casadocodigo.loja.service;
 
 import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
@@ -23,24 +28,45 @@ public class PagamentoService {
 	@Inject
 	private CompraDao compraDao;
 	
+	private static ExecutorService executor = Executors.newFixedThreadPool(50);
+	// 50: pool disponível
+	
 	@Inject
 	private PagamentoGateway pagamentoGateway;
 	
+	//@Suspended: contexto assíncrono e libera a thread principal
+	
+	
 	@POST
-	public Response pagar( @QueryParam("uuid") String uuid) {
+	public void pagar(@Suspended final AsyncResponse ar, 
+						@QueryParam("uuid") String uuid) {
 		
-		Compra compra = compraDao.buscaPorUuid(uuid);
+		String contextPath =context.getContextPath();
 		
-		pagamentoGateway.pagar( compra.getTotal() );
+		//   -> Lambda
+		executor.submit(() -> {
+			
+			try {
+				Compra compra = compraDao.buscaPorUuid(uuid);
+				pagamentoGateway.pagar( compra.getTotal() );
+
+				URI responseUri = UriBuilder.fromPath("http://localhost:8080" +
+						contextPath + "/index.xhtml")
+					.queryParam("msg", "Compra Realizada comSuceso")
+					.build();
+			
+				Response response = Response.seeOther(responseUri).build();				
+				ar.resume(response);				
+
+			} catch (Exception e) {
+
+				ar.resume(new WebApplicationException(e));
+				
+			}
+
+		});
+
 		
-		URI responseUri = UriBuilder.fromPath("http://localhost:8080" +
-					context.getContextPath() + "/index.xhtml")
-				.queryParam("msg", "Compra Realizada comSuceso")
-				.build();
-		
-		Response response = Response.seeOther(responseUri).build();
-		
-		return response;
 	}
 	
 }
